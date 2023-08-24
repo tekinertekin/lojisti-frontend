12bac573bcd8379decae983cd0ece4a47b154333
bumper-tekin-ertekin <tekin.ertekin@bumper.co>
2023-08-24 19:44:40 +0300

    text changed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -38,20 +38,20 @@ const Header = ({ t }: any) => {
     return (
       <>
         <CustomNavLinkSmall onClick={() => scrollTo("about")}>
-          <Span>{t("About")}</Span>
+          <Span>{t("Hakkımızda")}</Span>
         </CustomNavLinkSmall>
         <CustomNavLinkSmall onClick={() => scrollTo("mission")}>
-          <Span>{t("Mission")}</Span>
+          <Span>{t("Misyon")}</Span>
         </CustomNavLinkSmall>
         <CustomNavLinkSmall onClick={() => scrollTo("product")}>
-          <Span>{t("Product")}</Span>
+          <Span>{t("Ürün")}</Span>
         </CustomNavLinkSmall>
         <CustomNavLinkSmall
           style={{ width: "180px" }}
           onClick={() => scrollTo("contact")}
         >
           <Span>
-            <Button>{t("Contact")}</Button>
+            <Button>{t("Üye ol")}</Button>
           </Span>
         </CustomNavLinkSmall>
       </>
